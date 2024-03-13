7a4936ea19325cd62e458e9b27eb77322e6243c7
diopmorfall <morfall.papydiop99@gmail.com>
2024-03-13 12:13:27 +0000

    Components intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ProvaComponent } from './prova/prova.component';
+//* in v17 you need to import the components you need
+//* unlike the versions that have the app.module.ts
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProvaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
