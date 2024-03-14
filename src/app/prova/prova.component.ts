b742fe287ee4015f5d65dbc62e7bf908735fafde
diopmorfall <morfall.papydiop99@gmail.com>
2024-03-14 10:59:04 +0000

    Component lifecycle

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 
 import {MatSliderModule} from '@angular/material/slider';
 import {MatCardModule} from '@angular/material/card';
@@ -14,6 +14,38 @@ import {MatBadgeModule} from '@angular/material/badge';
   templateUrl: './prova.component.html',
   styleUrl: './prova.component.css'
 })
-export class ProvaComponent {
+export class ProvaComponent implements OnInit, AfterViewInit, DoCheck, AfterContentInit, AfterContentInit, AfterContentChecked, AfterViewChecked, OnDestroy {
+  constructor(){
+    console.log('constructor building the single component')
+  }
 
+  ngDoCheck(): void {
+    console.log('ngDoCheck, repeats some checks to look for changes')
+  }
+  
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked, runs after that the view is checked (text + components)');
+  }
+  
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit, runs after that the view is initialized (text + components)');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit, runs after that the content of the component is initialized (text)');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked, runs after that the content of the component is checked (text');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy, runs when the component is destroyed (for example, we render another one)');
+  }
+  
+  ngOnInit(): void {
+    console.log('ngOnInit, runs when the component is initialized')
+  }
+
+  //* with the lifecycle hooks, we can perform some actions during the loading of the components
 }
