eb2f3fb66d0accfb94e40b401df3b9d4ecfdbdbd
diopmorfall <morfall.papydiop99@gmail.com>
2024-03-14 14:51:38 +0000

    Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card'
+import {MatInputModule} from '@angular/material/input';
 import { ProvaComponent } from './prova/prova.component';
 //* in v17 you need to import the components you need
 //* unlike the older versions where you add them in app.module.ts
@@ -10,7 +10,7 @@ import { ProvaComponent } from './prova/prova.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProvaComponent, MatButtonModule],
+  imports: [RouterOutlet, ProvaComponent, MatButtonModule, MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -22,9 +22,17 @@ export class AppComponent implements OnInit{
       this.isDisabled = !this.isDisabled;
     }, 2000)
 
-    
+
   }
   title = 's2i-angular-tutorial';
   url1 = 'https://tse1.mm.bing.net/th?id=OIP.t5B55ZEKRYJ9nzL77VDgIwHaKT&pid=Api'
   url2 = 'https://tse2.mm.bing.net/th?id=OIP.UQ8B1FpdkDbR42AVhdAZXgHaLE&pid=Api'
+
+  onClick(e: any){
+    console.log('CLIC', e)
+  }
+
+  onInput(e: Event){ //* here we're getting the details of the event, including the data we need
+    console.log('Input', (<HTMLInputElement>e.target).value)
+  }
 }
